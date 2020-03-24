389a417cc77894d8ddc989967ac7669309000bc9
spoonLead <xuxlaevoleg@yanex.ru>
2020-03-24 09:26:00 +0400

    Refactor(Render): now work with SCENE

--- src/engine/render.js
+++ src/engine/render.js
@@ -1,7 +1,8 @@
-function render(objects){
-    for(var i = 0; i < objects.length; i++){
-      objects[i].draw();
-    }
-    player.draw(); //TODO debag: player dont draw without this string from the Camera.focusOn
-  }
-  
+function render(){
+  this.clearCanvas()
+  SCENE.draw()
+}
+
+function clearCanvas(){
+  screen.clearRect(0, 0, canvas.width, canvas.height)
+}
