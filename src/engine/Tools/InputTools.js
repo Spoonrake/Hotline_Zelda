498d292e09a395a54d41e5090a15898d9a077f4d
spoonLead <xuxlaevoleg@yandex.ru>
2020-03-26 17:14:35 +0400

    Refactor(InputTools): Refactored keyListener

--- src/engine/Tools/InputTools.js
+++ src/engine/Tools/InputTools.js
@@ -1,29 +1,28 @@
 
 //-----------------------KEYBOARD LISTENER---------------------//
+
 document.addEventListener('keydown', keyListener);
 document.addEventListener('keyup', keyListener);
-keyListener_downKeys = [];
+
+var keyListener_downKeys = [];
 
 function keyListener(event){
-if(event.type == 'keydown'){
-  if (!isElemInArr(keyListener_downKeys, event.keyCode)) 
-    keyListener_downKeys.push(event.keyCode);
-  }
-  else{
-    keyListener_downKeys.splice(keyListener_downKeys.indexOf(event.keyCode), 1);
-  }
+    if(event.type == 'keydown'){
+        if(!isElemInArr(keyListener_downKeys, event.keyCode)) 
+            addKeyToDownKeys(event.keyCode)
+    }
+    else
+        removeKeyFromDownKeys(event.keyCode)
 }
 
 
-
-function getLastDownedKey(){
-  this.lastDownedKey;
-  window.onkeydown = function(e){
-      this.lastDownedKey = e.keyCode
-  }
-  return this.lastDownedKey
+function addKeyToDownKeys(key){
+    keyListener_downKeys.push(key);
 }
 
+function removeKeyFromDownKeys(key){
+    keyListener_downKeys.splice(keyListener_downKeys.indexOf(key), 1);
+}
 //^^^^^^^^^^^^^^^^^^^^^^^KEYBOARD LISTENER^^^^^^^^^^^^^^^^^^^^^^^//
 
 
@@ -39,37 +38,35 @@ var mouseX = 0;
 var mouseY = 0;
 
 function mouseMoveListener(e){
-     if(event.target == canvas){
-          mouseX = event.clientX - event.target.offsetLeft
-          mouseY = event.clientY - event.target.offsetTop;
-     }
+    if(event.target == canvas){
+        mouseX = event.clientX - event.target.offsetLeft
+        mouseY = event.clientY - event.target.offsetTop;
+    }
 }
 
 
 function getMouseX(){
-     return mouseX
+    return mouseX
 }
 
 function getMouseY(){
-     return mouseY
+    return mouseY
 }
 
 //-----MOUSEDOWN-----
 var mouseDown = false;
 
 function mouseDownListener(e){
-     if(event.type == 'mousedown'){
-          mouseDown = true
-     }
-     if(event.type == 'mouseup'){
-          mouseDown = false
-     }
+    if(event.type == 'mousedown'){
+        mouseDown = true
+    }
+    if(event.type == 'mouseup'){
+        mouseDown = false
+    }
 }
 
 
 function isMouseDown(){
-     return mouseDown
+    return mouseDown
 }
 //^^^^^^^^^^^^^^^^^^^^^^^^^MOUSE LISTENER^^^^^^^^^^^^^^^^^^^^^^^^^//
-
-}
