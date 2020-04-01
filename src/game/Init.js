5e7434670753a91da2ada7c384cdb7b0115090a4
spoonLead <xuxlaevoleg@yandex.ru>
2020-04-01 15:30:39 +0400

    Refactor(Feat) Added Engine Init and moved there engine init from Init.js

--- src/game/Init.js
+++ src/game/Init.js
@@ -1,67 +1,15 @@
-window.onload = init;
-
-var canvas;
-var screen;
-
+//   DIFINITION FRAME PER SECOND FOR GAME
 const FPS = 30;
-const FRAMETIME = getFrameTimeForFPS(FPS);
-var CURRENTFRAME = 1;
 
-function getFrameTimeForFPS(fps){
-  return (1000/fps);
-}
 
-var SCENE;
+//   OBJECTS INITIALIZATION
 var PLAYER;
 var ENEMY;
-var CAMERA;
-var gameFlag = true;
 
 
 
-function init(){
-  canvasAndScreenDifintion()
-  CAMERA = new Camera()
-  SCENE = new Scene()
-  loadStartLevel()
-  gameLoop()       //игровой цикл
-}
-
-
-
-function canvasAndScreenDifintion(){
-  canvas = document.getElementById("canvas") //конвенция
-  screen = canvas.getContext("2d")
-}
-
-
-
-
+//   LOADING START LEVEL
 function loadStartLevel(){
   this.loadLevelTest()
 }
 
-
-
-
-
-async function gameLoop(){
-  while(true){
-      this.render()
-      this.gameLogic()
-      frameCounter()
-      await sleep(FRAMETIME)
-  }
-}
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function frameCounter(){
-  if(CURRENTFRAME < FPS)
-    CURRENTFRAME += 1;
-  else
-    CURRENTFRAME = 1;
-}
