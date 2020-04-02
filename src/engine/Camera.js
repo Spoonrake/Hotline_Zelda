f6029986f83e5fadf8dea492af9d58dc05a0c6b5
spoonLead <xuxlaevoleg@yandex.ru>
2020-04-02 16:02:04 +0400

    Refactor(Camera) Removed legacy code

--- src/engine/Camera.js
+++ src/engine/Camera.js
@@ -1,38 +1,23 @@
 class Camera{
-    constructor(x = canvas.width/2, y = canvas.height/2, speed = 1){
-      this.x = x
-      this.y = y
+    constructor(x = canvas.width/2, y = canvas.height/2, speed = 1, safetyMode = false){
       this.speed = speed;
-      this.mode;
       this.focus;
+      this.safetyMode = safetyMode
 
-      console.log(canvas)
       console.log("Camera set up")
       console.log("*Camera* x: " + this.x + ", y: " + this.y + ", speed: " + this.speed + ", safetyMode: " + this.safetyMode)
     }
 
 
     focusOn(obj){
-      if(this.mode != "focusOn")
-        this.mode = "focusOn";
-
       this.deltaX = canvas.width/2 - obj.width/2- obj.x;
-      this.x += obj.x + obj.width/2
       this.deltaY = canvas.height/2 - obj.height/2 - obj.y;
-      this.y += obj.y + obj.height/2
       mapScrolling(SCENE.objectsGroup, "x", this.deltaX, false);
       mapScrolling(SCENE.objectsGroup, "y", this.deltaY, false);
-
-      if(this.focus != obj){
-        this.focus = obj;
-      }
     }
 
 
     freeWalk(controlKeys = {left:65, right:68, up:87, down:83}){
-      this.focus = undefined;
-      this.mode = "freeWalk";
-
       if(isElemInArr(controlKeys.left, getDownedKeys()))
         this.goLeftFor(this.speed)
       if(isElemInArr(controlKeys.right, getDownedKeys()))
@@ -45,21 +30,17 @@ class Camera{
 
     goLeftFor(deltaX){
       mapScrolling(SCENE.objectsGroup, "x", deltaX, this.safetyMode)
-      this.x -= this.speed
     }
 
     goRightFor(deltaX){
       mapScrolling(SCENE.objectsGroup, "x", deltaX, this.safetyMode);
-      this.x += this.speed
     }
 
     goUpFor(deltaY){
       mapScrolling(SCENE.objectsGroup, "y", deltaY, this.safetyMode);
-      this.y -= this.speed
     }
     
     goDownFor(deltaY){
       mapScrolling(SCENE.objectsGroup, "y", deltaY, this.safetyMode);
-      this.y += this.speed
     }
-  }
+}
